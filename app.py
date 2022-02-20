c0d5be127c7fe7ab231bd275f08f43da8d592f38
rz119@duke.edu <rz119@duke.edu>
2022-02-20 15:50:55 -0500

    new

--- app.py
+++ app.py
@@ -14,8 +14,24 @@ app = dash.Dash(__name__)
 app.layout = html.Div([
 
     # Section title
-    html.H3("Section 1: Fetch & Display exchange rate historical data"),
-    html.H4("Select value for whatToShow:"),
+    html.H2("Section 1: Fetch & Display exchange rate historical data"),
+    html.H3("Select duration string:"),
+    html.Div(children=[
+        dcc.Input(id="duration-str", type="number", placeholder=1, style={'width': '365px'}),
+        dcc.Dropdown(["S", "W", "D"], "S", id='duration-unit')], style={'width': '365px'}),
+    html.H3("Select bar size setting:"),
+    html.Div(
+        dcc.Dropdown(["1 sec", "5 secs", "15 secs", "30 secs", "1 min", "2 mins", "3 mins",
+                      "5 mins", "15 mins", "30 mins", "1 hour", "1 day"], "1 hour", id='bar-size'),
+        style={'width': '365px'}),
+    html.H3("Select returning data:"),
+    html.Div(
+        dcc.RadioItems(
+            id='all-or-trade', options=[
+                {'label': 'return all data', 'value': False},
+                {'label': 'return data within regular trading hours', 'value': True}
+            ], value=1), style={'width': '400px'}),
+    html.H3("Select value for whatToShow:"),
     html.Div(
         dcc.Dropdown(
             ["TRADES", "MIDPOINT", "BID", "ASK", "BID_ASK", "ADJUSTED_LAST",
@@ -27,7 +43,7 @@ app.layout = html.Div([
         ),
         style = {'width': '365px'}
     ),
-    html.H4("Select value for endDateTime:"),
+    html.H3("Select value for endDateTime:"),
     html.Div(
         children = [
             html.P("You may select a specific endDateTime for the call to " + \
@@ -78,7 +94,7 @@ app.layout = html.Div([
         ]
     ),
 
-    html.H4("Enter a currency pair:"),
+    html.H3("Enter a currency pair:"),
     html.P(
         children=[
             "See the various currency pairs here: ",
@@ -143,10 +159,13 @@ app.layout = html.Div([
     #   of 'currency-input' at the time the button was pressed DOES get passed in.
     [State('currency-input', 'value'), State('what-to-show', 'value'),
      State('edt-date', 'date'), State('edt-hour', 'value'),
-     State('edt-minute', 'value'), State('edt-second', 'value')]
+     State('edt-minute', 'value'), State('edt-second', 'value'),
+     State('duration-str', 'value'), State('duration-unit', 'value'),
+     State('bar-size', 'value'), State('all-or-trade', 'value')]
 )
 def update_candlestick_graph(n_clicks, currency_string, what_to_show,
-                             edt_date, edt_hour, edt_minute, edt_second):
+                             edt_date, edt_hour, edt_minute, edt_second, duration_str,
+                             duration_unit, bar_size, all_or_trade):
     # n_clicks doesn't
     # get used, we only include it for the dependency.
 
@@ -175,28 +194,30 @@ def update_candlestick_graph(n_clicks, currency_string, what_to_show,
     # Some default values are provided below to help with your testing.
     # Don't forget -- you'll need to update the signature in this callback
     #   function to include your new vars!
-    # cph = fetch_historical_data(
-    #     contract=contract,
-    #     endDateTime='',
-    #     durationStr='30 D',       # <-- make a reactive input
-    #     barSizeSetting='1 hour',  # <-- make a reactive input
-    #     whatToShow=what_to_show,
-    #     useRTH=True               # <-- make a reactive input
-    # )
-    # # # Make the candlestick figure
-    # fig = go.Figure(
-    #     data=[
-    #         go.Candlestick(
-    #             x=cph['date'],
-    #             open=cph['open'],
-    #             high=cph['high'],
-    #             low=cph['low'],
-    #             close=cph['close']
-    #         )
-    #     ]
-    # )
-    # # # Give the candlestick figure a title
-    # fig.update_layout(title=('Exchange Rate: ' + currency_string))
+    durationStr = str(duration_str) + ' ' + str(duration_unit)
+
+    cph = fetch_historical_data(
+        contract=contract,
+        endDateTime='',
+        durationStr=durationStr,       # <-- make a reactive input
+        barSizeSetting=bar_size,  # <-- make a reactive input
+        whatToShow=what_to_show,
+        useRTH=all_or_trade               # <-- make a reactive input
+    )
+    # # Make the candlestick figure
+    fig = go.Figure(
+        data=[
+            go.Candlestick(
+                x=cph['date'],
+                open=cph['open'],
+                high=cph['high'],
+                low=cph['low'],
+                close=cph['close']
+            )
+        ]
+    )
+    # # Give the candlestick figure a title
+    fig.update_layout(title=('Exchange Rate: ' + currency_string))
     ############################################################################
     ############################################################################
 
@@ -204,22 +225,22 @@ def update_candlestick_graph(n_clicks, currency_string, what_to_show,
     ############################################################################
     # This block returns a candlestick plot of apple stock prices. You'll need
     # to delete or comment out this block and use your currency prices instead.
-    df = pd.read_csv(
-        'https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv'
-    )
-    fig = go.Figure(
-        data=[
-            go.Candlestick(
-                x=df['Date'],
-                open=df['AAPL.Open'],
-                high=df['AAPL.High'],
-                low=df['AAPL.Low'],
-                close=df['AAPL.Close']
-            )
-        ]
-    )
+    # df = pd.read_csv(
+    #     'https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv'
+    # )
+    # fig = go.Figure(
+    #     data=[
+    #         go.Candlestick(
+    #             x=df['Date'],
+    #             open=df['AAPL.Open'],
+    #             high=df['AAPL.High'],
+    #             low=df['AAPL.Low'],
+    #             close=df['AAPL.Close']
+    #         )
+    #     ]
+    # )
 
-    currency_string = 'default Apple price data fetch'
+    # currency_string = 'default Apple price data fetch'
     ############################################################################
     ############################################################################
 
